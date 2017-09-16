11ca8ebe845e2d093c80e0ece3b3e93348251c4a
Bappa Sarkar <find.bappa@gmail.com>
2017-09-16 12:19:38 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,12 @@
 # Fibonacci numbers till position n.
 
-n = 12
+n = input("what is the nth position? ")
+n = int(n)
 current = 1
 next = 1
-msg = "Fibonacci numbers till position " + repr(n) + ": "
+msg = "Fibonacci numbers till position " + str(n) + ": "
 print(msg)
-for i in range(1, n):
+for i in range(0, n):
     print(current)
     temp_current = current
     current = next
